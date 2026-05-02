be2b60a0237d389afedb94ea2b9dd877815a3232
Wynn <willmantegar77@gmail.com>
2026-05-02 13:51:06 +0700

    Update proses_tambah.php

--- Backend/proses_tambah.php
+++ Backend/proses_tambah.php
@@ -7,15 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $price = $_POST['price'];
     $stock = $_POST['stock'];
 
-    // Proses upload gambar
     $nama_file = $_FILES['image']['name'];
     $tmp_file  = $_FILES['image']['tmp_name'];
     
-    // Simpan ke folder images/ di dalam folder Backend
-    $path = "images/" . time() . "_" . basename($nama_file); // Tambah time() biar nama unik
-    
+    $path = "images/" . time() . "_" . basename($nama_file);
+
     if (move_uploaded_file($tmp_file, $path)) {
-        // Masukkan ke database
         $sql = "INSERT INTO products (name, price, stock, category, image) VALUES ('$name', '$price', '$stock', '$category', '$path')";
         
         if ($conn->query($sql) === TRUE) {
